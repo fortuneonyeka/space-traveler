2ab1c576b28e358f466996c9534a5c9f1ab37cdc
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-13 21:48:49 +0100

    add myprofile view

---
+++ src/components/MyProfile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyProfile = () => (
+  <div className="w-full max-w-7xl flex justify-between p-4 items-center">
+    <p>Coming soon</p>
+  </div>
+);
+
+export default MyProfile;
