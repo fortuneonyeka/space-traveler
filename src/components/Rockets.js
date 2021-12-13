53106889e78688cf9d93da0f3db4f938408ad265
Lino09 <onosmx@gmail.com>
2021-12-13 13:19:43 -0600

    Added navigation and Navbar

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Rockets = () => (
-  <h1>Hello rockets</h1>
+  <div className="w-full max-w-7xl flex justify-between p-4 items-center">
+
+    <h1>Hello rockets</h1>
+  </div>
 );
 
 export default Rockets;
--- src/index.js
+++ src/index.js
@@ -13,12 +13,14 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Rockets />} />
-        <Route path="missions" element={<Missions />} />
-        {/* <Route path="my-profile" element={<MyProfile />} /> */}
-      </Routes>
+      <div className="w-full flex items-center flex-col">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="missions" element={<Missions />} />
+          {/* <Route path="my-profile" element={<MyProfile />} /> */}
+        </Routes>
+      </div>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
