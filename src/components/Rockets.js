2e44baa2c5fe22d47e941d052aa69a355b696e7a
Lino09 <onosmx@gmail.com>
2021-12-13 12:01:01 -0600

    Add file structure

---
+++ src/components/Rockets.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Rockets = () => (
+  <h1>Hello rockets</h1>
+);
+
+export default Rockets;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello </h1>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="missions" element={<Missions />} />
+        {/* <Route path="my-profile" element={<MyProfile />} /> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
