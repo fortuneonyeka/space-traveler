64f230340060e37fb304df2367b4ac2878921915
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-13 22:48:06 +0100

    update missions reducers

--- src/redux/Missions/missions.js
+++ src/redux/Missions/missions.js
@@ -4,7 +4,7 @@ const LOAD = "space-traveler/missions/LOAD";
 const RESERVE = "space-traveler/missions/RESERVE";
 const CANCEL = "space-traveler/missions/CANCEL";
 
-export default function reducer() {
+export default function missionsReducer() {
  switch (action.type) {
     case LOAD:
       return state;
