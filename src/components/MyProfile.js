f59343a7e429fdde8f4a565371089e42a64fe56c
Lino09 <onosmx@gmail.com>
2021-12-14 11:35:13 -0600

    Style the rockets side of my-profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import rocketIcon from '../assets/icon-rocket.svg';
 
 const MyProfile = () => {
   const rocketList = useSelector((state) => state.rocketReducer);
   return (
     <div className="w-full max-w-7xl flex flex-col items-center justify-start p-4">
-      <div className="w-full grid grid-cols-2 gap-16">
-        <div className="border w-full">
+      <div className="w-full grid grid-cols-2 gap-8">
+        <div className="w-full">
           <h1 className="w-full text-3xl text-center text-indigo-500 font-bold">Missions</h1>
           <ul>
             <li>Missionslist</li>
           </ul>
         </div>
-        <div className="border w-full">
-          <h1 className="w-full text-3xl text-center text-indigo-500 font-bold">Rockets</h1>
-          <ul>
+        <div className="w-full">
+          <h1 className="w-full text-3xl text-center text-indigo-500 font-bold"> Reserved Rockets</h1>
+          <ul className="space-y-8 py-8">
+            { ((rocketList.filter((rocket) => rocket.reserved).length) === 0) ? (
+              <h2 className="text-2xl font-semibold text-center">You haven&apos;t Reserved any rocket yet!</h2>
+            ) : ('')}
             {rocketList.filter((rocket) => rocket.reserved).map((rocket) => (
-              <li className="text-xl" key={rocket.id}>{rocket.name}</li>
+              <li className="text-xl bg-gray-100 p-4 grid grid-cols-3 items-center justify-items-center" key={rocket.id}>
+                <span className="text-xl uppercase font-semibold">{rocket.name}</span>
+                <img src={rocketIcon} alt="rocket icon" />
+                <div className="relative rounded-full group">
+                  <img
+                    className="w-6 h-6"
+                    src="https://img.icons8.com/material-outlined/50/000000/info.png"
+                    alt="more info"
+                  />
+                  <span
+                    className="absolute w-80 hidden group-hover:block bg-indigo-100 z-20 p-4 border border-indigo-600 rounded-lg right-8 -top-20 text-sm font-bold"
+                  >
+                    {rocket.description}
+                  </span>
+                </div>
+              </li>
             ))}
           </ul>
         </div>
