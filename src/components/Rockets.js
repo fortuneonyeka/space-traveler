567efb9d5aac920e2d6396a5ad26e1fa3233491a
Lino09 <onosmx@gmail.com>
2021-12-14 10:20:27 -0600

    Add function to reserve and cancel rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchRocketsAsync } from '../redux/Rockets/rockets';
+import { fetchRocketsAsync, reserveRocket, cancelReservedRocket } from '../redux/Rockets/rockets';
 
 const Rockets = () => {
   const dispatch = useDispatch();
@@ -8,35 +8,55 @@ const Rockets = () => {
   useEffect(() => {
     if (rocketList.length === 0) dispatch(fetchRocketsAsync());
   }, []);
+  const reserveIt = (id) => {
+    dispatch(reserveRocket(id));
+  };
+  const cancelIt = (id) => {
+    dispatch(cancelReservedRocket(id));
+  };
   return (
     <div className="w-full max-w-7xl flex flex-col items-center">
       <h1 className="text-5xl font-bold mt-8 mb-16">Travel in one of our amazing Rockets</h1>
       <ul className="w-full space-y-8">
         {rocketList.map((rocket) => (
-          <li className="flex space-x-8 shadow" key={rocket.id}>
-            <img src={rocket.img} alt="rocket" className="h-48 w-60" />
-            <div className="flex flex-col justify-between pb-4 items-start">
+          <li className="flex shadow" key={rocket.id}>
+            <div className="relative overflow-hidden w-96">
+              <img src={rocket.img} alt="rocket" className="" />
+              {(rocket.reserved) ? (
+                <span
+                  className="px-8 py-1 bg-yellow-400 font-bold absolute top-4 -right-8 transform rotate-45"
+                >
+                  Reserved
+                </span>
+              ) : '' }
+            </div>
+            <div className="flex flex-col justify-between pb-4 items-start w-3/5 px-8">
               <h2 className="text-3xl font-bold">{rocket.name}</h2>
-              <span>
-                { rocket.reserved }
-                Lorem, ipsum dolor sit
-                amet consectetur adipisicing elit.
-                Labore accusantium corporis quasi quo in voluptatem nisi.
-                Rerum ipsam eius error.
-                Lorem, ipsum dolor sit
-                amet consectetur adipisicing elit.
-                Labore accusantium corporis quasi quo in voluptatem nisi.
-                Rerum ipsam eius error.
-                Lorem, ipsum dolor sit
-                amet consectetur adipisicing elit.
-                Labore accusantium
+              <span className="trext-sm tracking-tight leading-tight">
+                {rocket.description}
               </span>
               <p>
                 Type:
                 {' '}
                 {rocket.type}
               </p>
-              <button className="border rounded-lg px-4 py-1 hover:bg-gray-50" type="button">Reserve</button>
+              {(rocket.reserved) ? (
+                <button
+                  onClick={() => cancelIt(rocket.id)}
+                  className="border border-indigo-400 text-indigo-600 bg-gray-100 rounded-lg px-6 py-2 hover:bg-white"
+                  type="button"
+                >
+                  Cancel
+                </button>
+              ) : (
+                <button
+                  onClick={() => reserveIt(rocket.id)}
+                  className="border border-indigo-400 text-indigo-600 bg-gray-100 rounded-lg px-6 py-2 hover:bg-white"
+                  type="button"
+                >
+                  Reserve
+                </button>
+              )}
             </div>
           </li>
         ))}
--- src/redux/Rockets/rockets.js
+++ src/redux/Rockets/rockets.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const FETCH = 'space-traveler/rockets/FETCH';
+const RESERVE = 'space-traveler/rockets/RESERVE';
+const CANCEL_RESERVE = 'space-traveler/rockets/CANCEL_RESERVE';
 const URL = 'https://api.spacexdata.com/v3/rockets';
 
 export const fetchRockets = (toPurge) => {
@@ -10,6 +12,8 @@ export const fetchRockets = (toPurge) => {
       name: el.rocket_name,
       type: el.rocket_type,
       img: el.flickr_images[0],
+      reserved: false,
+      description: el.description,
     }
   ));
   return ({
@@ -24,10 +28,38 @@ export const fetchRocketsAsync = () => (function caller(dispatch) {
   });
 });
 
+export const reserveRocket = (payload) => ({
+  type: RESERVE,
+  payload,
+});
+
+export const cancelReservedRocket = (payload) => ({
+  type: CANCEL_RESERVE,
+  payload,
+});
+
 const rocketReducer = (state = [], { type, payload }) => {
   switch (type) {
     case FETCH:
       return payload;
+    case RESERVE: {
+      const newState = state.map((rocket) => {
+        if (rocket.id !== payload) {
+          return rocket;
+        }
+        return { ...rocket, reserved: true };
+      });
+      return newState;
+    }
+    case CANCEL_RESERVE: {
+      const newState = state.map((rocket) => {
+        if (rocket.id !== payload) {
+          return rocket;
+        }
+        return { ...rocket, reserved: false };
+      });
+      return newState;
+    }
     default:
       return state;
   }
