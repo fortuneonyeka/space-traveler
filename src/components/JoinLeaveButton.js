6f9761d9ded376977eb8cf1f5df09ce86cac7dad
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-14 22:08:43 +0100

    add width to buttons

--- src/components/JoinLeaveButton.js
+++ src/components/JoinLeaveButton.js
@@ -22,7 +22,7 @@ const JoinLeaveButton = ({ reserved, id }) => {
       <Button
         className="inline-flex items-center px-2.5 py-1.5 border
     border-red-600 text-xs font-medium rounded shadow-sm text-red-500
-    bg-white-200 w-36 h-10 hover:bg-indigo-300 h-15"
+    bg-white-200 w-30 h-10 hover:bg-indigo-300 h-15"
         onClick={leaveMissionToStore}
       >
         Leave Mission
@@ -33,7 +33,7 @@ const JoinLeaveButton = ({ reserved, id }) => {
     <Button
       className="inline-flex items-center px-2.5 py-1.5 border
     border-transparent text-xs font-medium rounded shadow-sm text-black
-    bg-gray-200 w-36 h-10 hover:bg-green-300"
+    bg-gray-200 w-30 h-10 hover:bg-green-300"
       onClick={addMissionToStore}
     >
       Join Mission
