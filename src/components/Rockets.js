fed0b4b10a363ce49321174607b5b6c4e53d73a6
Lino09 <onosmx@gmail.com>
2021-12-13 23:49:39 -0600

    API fetching implemented

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRocketsAsync } from '../redux/Rockets/rockets';
 
-const Rockets = () => (
-  <div className="w-full max-w-7xl flex justify-between p-4 items-center">
-
-    <h1>Hello rockets</h1>
-  </div>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rocketList = useSelector((state) => state.rocketReducer);
+  useEffect(() => {
+    if(rocketList.length === 0) dispatch(fetchRocketsAsync()); //eslint-disable-line
+  }, []);
+  return (
+    <div className="w-full max-w-7xl flex flex-col items-center">
+      <h1>Hello rockets</h1>
+      <ul className="w-full space-y-4">
+        {rocketList.map((rocket) => (
+          <li className="flex" key={rocket.id}>
+            <img src={rocket.img} alt="rocket" className="h-48 w-60" />
+            <div>
+              <h2>{rocket.name}</h2>
+              <span>{rocket.reserved}</span>
+              <p>{rocket.type}</p>
+              <button type="button">Reserve</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Rockets;
--- src/redux/Rockets/rockets.js
+++ src/redux/Rockets/rockets.js
@@ -1,4 +1,28 @@
+import axios from 'axios';
+
 const FETCH = 'space-traveler/rockets/FETCH';
+const URL = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRockets = (toPurge) => {
+  const payload = toPurge.map((el) => (
+    {
+      id: el.id,
+      name: el.rocket_name,
+      type: el.rocket_type,
+      img: el.flickr_images[0],
+    }
+  ));
+  return ({
+    type: FETCH,
+    payload,
+  });
+};
+
+export const fetchRocketsAsync = () => (function caller(dispatch) {
+  axios.get(URL).then((response) => {
+    dispatch(fetchRockets(response.data));
+  });
+});
 
 const rocketReducer = (state = [], { type, payload }) => {
   switch (type) {
