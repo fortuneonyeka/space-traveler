090ead8a03665cca399a6e9569e62e2ee0082fde
Lino09 <onosmx@gmail.com>
2021-12-14 10:49:19 -0600

    Add boileplate to show  reservations

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div className="w-full max-w-7xl flex justify-between p-4 items-center">
-    <p>Coming soon</p>
-  </div>
-);
+const MyProfile = () => {
+  const rocketList = useSelector((state) => state.rocketReducer);
+  return (
+    <div className="w-full max-w-7xl flex flex-col items-center justify-start p-4">
+      <div className="w-full grid grid-cols-2 gap-16">
+        <div className="border w-full">
+          <h1 className="w-full text-3xl text-center text-indigo-500 font-bold">Missions</h1>
+          <ul>
+            <li>Missionslist</li>
+          </ul>
+        </div>
+        <div className="border w-full">
+          <h1 className="w-full text-3xl text-center text-indigo-500 font-bold">Rockets</h1>
+          <ul>
+            {rocketList.filter((rocket) => rocket.reserved).map((rocket) => (
+              <li className="text-xl" key={rocket.id}>{rocket.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
