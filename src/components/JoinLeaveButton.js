07cded2ae839cfbaf84fe71add4f4375cb5153e6
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-14 18:24:14 +0100

    improve button style

--- src/components/JoinLeaveButton.js
+++ src/components/JoinLeaveButton.js
@@ -1,7 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import { useDispatch } from 'react-redux';
 import { PropTypes } from 'prop-types'; // eslint-disable-line import/no-extraneous-dependencies
-import React from 'react';
 import { joinMission, leaveMission } from '../redux/Missions/missions';
 
 const JoinLeaveButton = ({ reserved, id }) => {
@@ -23,7 +22,7 @@ const JoinLeaveButton = ({ reserved, id }) => {
       <Button
         className="inline-flex items-center px-2.5 py-1.5 border
     border-red-600 text-xs font-medium rounded shadow-sm text-red-500
-    bg-white-200 w-36 hover:bg-red-600"
+    bg-white-200 w-36 h-10 hover:bg-indigo-300 h-15"
         onClick={leaveMissionToStore}
       >
         Leave Mission
@@ -34,7 +33,7 @@ const JoinLeaveButton = ({ reserved, id }) => {
     <Button
       className="inline-flex items-center px-2.5 py-1.5 border
     border-transparent text-xs font-medium rounded shadow-sm text-black
-    bg-gray-200 w-36 hover:bg-green-300"
+    bg-gray-200 w-36 h-10 hover:bg-green-300"
       onClick={addMissionToStore}
     >
       Join Mission
