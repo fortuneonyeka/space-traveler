385b808579f07a460a639ee0854cf8e45de2a80d
Lino09 <onosmx@gmail.com>
2021-12-14 15:25:53 -0600

    Navbar improvement

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,32 +4,33 @@ import logo from '../assets/logo.png';
 
 const NavBar = () => (
   <div className="w-full flex justify-center">
-    <div className="w-full max-w-7xl flex justify-between border-b-2 p-4 items-center">
+    <div className="w-full max-w-7xl flex justify-between border-b-2 p-4 items-center mb-8">
       <div className="flex space-x-4 items-center">
         <img className="w-12 h-12" src={logo} alt="logo" />
         <h1>Space Traveler</h1>
       </div>
 
-      <nav className="space-x-4 items-center">
+      <nav className="space-x-4 items-center flex relative">
         <NavLink
-          className="hover:text-white hover:bg-gray-400 p-4 hover:transform hover:translate-y-16"
+          className="block transition-all duration-500 p-2 rounded-t-lg"
           to="/"
         >
           Home
         </NavLink>
         <NavLink
-          className="hover:text-white hover:bg-gray-400"
+          className="block transition-all duration-500 p-2 rounded-t-lg"
           to="/Missions"
         >
           Missions
         </NavLink>
         <NavLink
-          className="hover:text-white hover:bg-gray-400"
+          className="block transition-all duration-500 p-2 rounded-t-lg"
           to="/my-profile"
         >
           {' '}
           My profile
         </NavLink>
+        <div className="transition-all duration-500 indicator w-20 h-8 bg-indigo-100 border-t border-l border-r border-indigo-500 absolute -bottom-5 rounded-t-lg" />
       </nav>
     </div>
   </div>
